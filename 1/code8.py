a966e0d363585dabeecdbe4bf0ffc9dbf8a780f2
friyantismile <friyantismile@gmail.com>
2019-07-18 16:53:25 +0800

    code8

--- 1/code8.py
+++ 1/code8.py
@@ -1,32 +1,25 @@
-import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 x = [None] * 10
-
+y = [None] * 10
 
 for i in range(len(x)):
-    x[i] = i+1
+    x[i] = i
+    y[i] = np.exp(-0.2*i)*np.cos(i) 
 
-fx1 = [None] * 10
-for i in range(len(fx1)):
-    fx1[i] = (3 * np.power(x[i],2) - 4.5 / x[i]) * np.exp(-x[i] / 1.3)
 
-fx2 = [None] * 10
-for i in range(len(fx2)):
-    fx2[i] = 5*np.sin(5*x[i])*np.exp(-x[i])
+fig, (ax1, ax2, ax3) = plt.subplots(3)
 
-fig, (ax1, ax2) = plt.subplots(2)
+ax1.axis([-1,1,-1,1])
+ax1.plot(x,y,"b")
+ 
+ax2.plot(x,y,"b")
+ax2.axis([0,2.3,4,5])
 
-ax1.plot(x,fx1,"r")
-ax1.plot(x,fx2,"b", "5*np.sin(5*x[i])*np.exp(-x[i])")
-ax1.axis([0.5,5,-5,5])
-# ax1.set_xlim([0.5,5])
-# ax1.set_ylim([-5,5])
-
-ax2.plot(x,x,"b", "Benchmark")
-ax2.plot(x,np.power(x,2),"k--", "O(x^2)")
+ax3.axis([-1,1,0,5])
+ax3.loglog(x, y)
+ax3.plot(x,y,"b")
 
 plt.show()
